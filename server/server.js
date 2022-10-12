92353d58065b1adb3e0d84edfd9a3d4deda0a950
meshack-osagie <68642751+Meshack-Osagie@users.noreply.github.com>
2022-10-12 04:44:12 +0100

    mongoDb connect

--- server/server.js
+++ server/server.js
@@ -2,9 +2,22 @@ const express = require("express");
 const mongoose = require('mongoose')
 const app = express();
 
+const URI = "mongodb+srv://mesh:kainos4Dunamis@wakabook1.4kfos.mongodb.net/?retryWrites=true&w=majority"
+
+async function connect() {
+    try {
+        await mongoose.connect(URI);
+        console.log("connected to mongoDb")
+    } catch(error){
+        console.log(error);
+    }
+}
+
+
+connect();
 
 app.get("/api", (req, res) => {
-    res.json({"users":["userOne", "userTwo", "userThree", "userFour", "userFive"]
+    res.json({"users":["userOne", "userTwo", "userThree", "userFour", "userFive", "userSix"]
     })
 })
 app.post('/', (req, res) => {
